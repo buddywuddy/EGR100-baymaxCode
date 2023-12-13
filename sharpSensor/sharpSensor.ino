87013d9dfe2310b2cf23fb1ad5305d628ab37d27
buddy <buddywuddy.code@gmail.com>
2023-12-12 20:36:57 -0500

    sharp sensors work AF

--- sharpSensor/sharpSensor.ino
+++ sharpSensor/sharpSensor.ino
@@ -1,6 +1,9 @@
 #include <Servo.h>
 int dir;
 int opposite;
+bool runOnce = false;
+int speed = 150;
+bool turned = false;
 
 // ANALOG PORTS
 int movedBack = 0;
@@ -23,7 +26,7 @@ int F_stripeReading = analogRead(F_stripePin)*4;
 int F_stripeThreshold = 1000;
 String F_color = "";
 
-int stripeCount = 0;
+int circleCount = 0;
 bool inRoom;
 
 int R_flamePin = 4;
@@ -51,7 +54,7 @@ int D2 = 7;
 int forward = LOW;
 int backward = HIGH;
 
-// MOVEMENT FUNCTIONS
+// MOVEMENT FUNCTIONS --------------------------------------------------------------------------
 void stop() {
   analogWrite(S1, 0);
   analogWrite(S2, 0);
@@ -88,10 +91,10 @@ void tLeft(int speed) {
 void touch(){
   touchState = digitalRead(touchPin);
   if (touchState == HIGH) {
-    mForward(175);
+    mForward(speed);
   }
   else if (touchState == LOW) {
-    mBackward(175);
+    mBackward(speed);
     delay(400);
 
   }
@@ -102,40 +105,44 @@ void wallFollowing(int direction){
   if (direction == 0){
     distanceL = analogRead(R_sharpPin);
     if (distanceL < 125){
-      tLeft(175);
+      tLeft(speed);
       delay(100);
+      mForward(75);
+      delay(75);
    } else {
-     mForward(175);
+     mForward(speed);
     }
 
     // Check left sensor for wall
-    if (distanceL > 185) {
+    if (distanceL > 200) {
       // Wall on the left, turn right
-      tRight(175);
+      tRight(speed);
       delay(100);
     } else {
      // No wall on the left, move forward
-     mForward(175);
+     mForward(speed);
     }
     touch();
 
   } else if (direction == 1){
     distanceR = analogRead(L_sharpPin);
     if (distanceR < farThreshold){
-      tRight(175);
+      tRight(speed);
       delay(100);
+      mForward(75);
+      delay(75);
     } else {
-      mForward(175);
+      mForward(speed);
     }
 
     // Check left sensor for wall
     if (distanceR > closeThreshold) {
       // Wall on the left, turn right
-      tLeft(175);
+      tLeft(speed);
       delay(100);
     } else {
       // No wall on the left, move forward
-      mForward(175);
+      mForward(speed);
     }
     touch();
   }
@@ -158,12 +165,39 @@ void stripe(){
   }
 
   if (F_color.equals(B_color) && B_color.equals("WHITE")){
-    stripeCount++;
-  } else if (F_color.equals("BLACK") && B_color.equals("WHITE")){
-    inRoom = true;
-  } else if (F_color.equals("WHITE") && B_color.equals("BLACK")){
-    inRoom = false;
-  }
+    circleCount++;
+  } 
+  // else if (F_color.equals("BLACK") && B_color.equals("WHITE") && circleCount == 0){
+  //   inRoom = true;
+  //   mForward(100);
+  //   delay(250);
+
+  //   // if (dir == 0 && turned == false){
+  //   //   mForward(100);
+  //   //   delay(500);
+
+  //   //   tRight(150);
+  //   //   delay(750);
+  //   //   mForward(100);
+  //   //   delay(500);
+  //   //   tRight(150);
+  //   //   delay(750);
+  //   //   mForward(100);
+  //   //   delay(500);
+  //   //   tRight(150);
+  //   //   delay(750);
+  //   //   mForward(100);
+  //   //   delay(500);
+  //   //   tRight(150);
+  //   //   delay(750);
+
+  //   //   turned = true;
+  //   // }
+  // } else if (F_color.equals("WHITE") && B_color.equals("BLACK") && circleCount == 0){
+  //   inRoom = false;
+  //   mForward(100);
+  //   delay(250);
+  // }
 }
 
 void servoTurn(){
@@ -181,30 +215,46 @@ void servoTurn(){
 void flame(){
   lFlame = analogRead(L_flamePin);
   rFlame = analogRead(R_flamePin);
-  
-  if (lFlame < flameThreshold){
-    tLeft(100);
-    if (lFlame < fireThreshold){
-      stop();
-      // digitalWrite(fanPin, HIGH);
-      servoTurn();
-      delay(1000);
-    }
-  }
 
-  if (rFlame < flameThreshold){
-    tRight(100);
-    if (rFlame < fireThreshold){
+  if (lFlame < flameThreshold || rFlame < flameThreshold){
+    digitalWrite(fanPin, HIGH);
+    if (lFlame < rFlame){
+      tLeft(75);
+    } else if (rFlame < lFlame){
+      tRight(75);
+    }
+
+    if (lFlame < fireThreshold && rFlame < fireThreshold){
       stop();
-      // digitalWrite(fanPin, HIGH);
       servoTurn();
       delay(1000);
     }
   }
+  
+  // if (lFlame < flameThreshold){
+  //   tLeft(100);
+  //   if (lFlame < fireThreshold){
+  //     stop();
+  //     // digitalWrite(fanPin, HIGH);
+  //     servoTurn();
+  //     delay(1000);
+  //   }
+  // }
+
+  // if (rFlame < flameThreshold){
+  //   tRight(100);
+  //   if (rFlame < fireThreshold){
+  //     stop();
+  //     // digitalWrite(fanPin, HIGH);
+  //     servoTurn();
+  //     delay(1000);
+  //   }
+  // }
 
   motorServo.write(90);
   digitalWrite(fanPin, LOW);
 }
+
 
 //-------------------------------------------------------------
 void setup() {
@@ -231,26 +281,51 @@ void setup() {
   distanceR = analogRead(L_sharpPin);
   distanceL = analogRead(R_sharpPin);
 
-  if (distanceR > closeThreshold){
+  if (distanceR > closeThreshold){ //if wall on right start with right wall following
     dir = 1;
     opposite = 0;
-  } else if (distanceL > 185 ){
+  } else if (distanceL > 185 ){ //if wall on left start wil left wall following
     dir = 0;
     opposite = 1;
   }
 
-  mForward(150);
-  delay(500);
+  mForward(150);    //initial move off of the circle;
+  delay(1000);
 
 }
 
 
 void loop() {
+  // wallFollowing(dir); //start wall following based on the position at the start
   stripe();
-  wallFollowing(dir);
 
-  if(stripeCount != 0){
-    wallFollowing(opposite);
+  if (circleCount == 0){
+    wallFollowing(dir);
+  }
+
+  // if (inRoom == false){
+  //   wallFollowing(dir);
+  // } else if (inRoom == true){
+  //   wallFollowing(opposite);
+  // }
+
+  if(circleCount != 0){
+    if (runOnce == false){
+      mBackward(150);
+      delay(250);
+      if (dir == 1){
+        tLeft(100);
+        delay(400);
+      } else if (dir == 0){
+        tRight(100);
+        delay(400);
+        mForward(100);
+        delay(400);
+      }
+      runOnce = true;
+    }
+    wallFollowing(opposite); //when the circle has been passed, start wall following in the opposite direction
+
   }
 
   flame();
